766edaf5d9e18c9917e5ff6de3978fd8936c956e
jyash0571 <107945736+jyash0571@users.noreply.github.com>
2022-06-24 10:47:33 +0530

    Add files via upload

---
+++ js/send-whatsapp.js
@@ -0,0 +1,29 @@
+function sendWhatsapp(){
+    if(document.getElementById("form-name")=="")
+        return;
+    if(document.getElementById("form-contact")=="")
+        return;
+    if(document.getElementById("form-contact")=="")
+        return;
+    
+    var message = "Name : ";
+    console.log(message);
+    message += document.getElementById("form-name").value;
+    console.log(message);
+
+    message += "\nContact : ";
+    console.log(message);
+    message += document.getElementById("form-contact").value;
+    console.log(message);
+    message += "\nClass : ";
+    console.log(message);
+    message += document.getElementById("form-class").value;
+    console.log(message);
+    
+    link = 'http://wa.me/919807363146?text=' + encodeURI(message);
+    
+    window.open(link, '_blank');
+}
+var submit = document.getElementById("send-form");
+
+submit.addEventListener("click",sendWhatsapp);
